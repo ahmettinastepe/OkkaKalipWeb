ea3e72458f93051528e018bc4cd87c155cdcd950
ahmettinastepe <ahmettinastepe0@gmail.com>
2020-03-13 19:57:52 +0300

    management panel added

--- OkkaKalipWeb.UI/Controllers/AdminController.cs
+++ OkkaKalipWeb.UI/Controllers/AdminController.cs
@@ -1,17 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using OkkaKalipWeb.Business.Abstract;
+using OkkaKalipWeb.Entities;
+using OkkaKalipWeb.UI.Models;
 
 namespace OkkaKalipWeb.UI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductService _productService;
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
         public IActionResult Index()
         {
             ViewBag.SelectedMenu = RouteData.Values["controller"];
+
+            return View(new ProductListModel()
+            {
+                Products = _productService.GetAll()
+            });
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel model)
+        {
+            var entity = new Product()
+            {
+                Name = model.Name,
+                ImageUrl = model.ImageUrl,
+                Description = model.Description,
+                Price = model.Price
+            };
+            _productService.Create(entity);
+
+            return Redirect("Index");
         }
     }
 }
